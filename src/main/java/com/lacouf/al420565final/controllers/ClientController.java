bcd34d64b3de9df6155c6556552fa6882af5f607
OussaKab <oussamakably@gmail.com>
2021-12-10 17:26:55 -0500

    test

--- src/main/java/com/lacouf/al420565final/controllers/ClientController.java
+++ src/main/java/com/lacouf/al420565final/controllers/ClientController.java
@@ -1,6 +1,7 @@
 package com.lacouf.al420565final.controllers;
 
 import com.lacouf.al420565final.utils.ClientFactory;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/clients")
+@CrossOrigin(origins = "http://localhost:3000")
 public class ClientController {
 
     private final List<Client> clients = ClientFactory.getClients();
@@ -23,11 +25,6 @@ public class ClientController {
     @GetMapping("/hommes")
     public List<Client> getHommes(){
         return clients.stream().filter(c -> "M".equalsIgnoreCase(c.getGender())).collect(Collectors.toList());
-    }
-
-    @GetMapping("/femmes")
-    public List<Client> getFemmes(){
-        return clients.stream().filter(c -> "F".equalsIgnoreCase(c.getGender())).collect(Collectors.toList());
     }
 
     @GetMapping("/ontariens")
