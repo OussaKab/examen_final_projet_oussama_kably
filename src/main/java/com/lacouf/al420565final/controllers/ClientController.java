8a28f17a86506fbd6e9f79c3ee208342822f935b
OussaKab <oussamakably@gmail.com>
2021-12-10 16:37:51 -0500

    commit initial

---
+++ src/main/java/com/lacouf/al420565final/controllers/ClientController.java
@@ -0,0 +1,37 @@
+package com.lacouf.al420565final.controllers;
+
+import com.lacouf.al420565final.utils.ClientFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.lacouf.al420565final.model.Client;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/clients")
+public class ClientController {
+
+    private final List<Client> clients = ClientFactory.getClients();
+
+    @GetMapping
+    public List<Client> getAll(){
+        return clients;
+    }
+
+    @GetMapping("/hommes")
+    public List<Client> getHommes(){
+        return clients.stream().filter(c -> "M".equalsIgnoreCase(c.getGender())).collect(Collectors.toList());
+    }
+
+    @GetMapping("/femmes")
+    public List<Client> getFemmes(){
+        return clients.stream().filter(c -> "F".equalsIgnoreCase(c.getGender())).collect(Collectors.toList());
+    }
+
+    @GetMapping("/ontariens")
+    public List<Client> getOntariens(){
+        return clients.stream().filter(c -> "ON".equalsIgnoreCase(c.getProvince())).collect(Collectors.toList());
+    }
+}
